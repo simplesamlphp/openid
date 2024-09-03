171cd7f36d96b73da96f5b725ad2ac3b1254cc9e
Marko Ivančić <marko.ivancic@srce.hr>
2024-09-03 15:44:45 +0200

    Make JwksDecorator readonly

--- src/Jwks/JwksDecorator.php
+++ src/Jwks/JwksDecorator.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace SimpleSAML\OpenID\Jwks;
 
 use Jose\Component\Core\JWKSet;
+use JsonSerializable;
 
-class JwksDecorator
+readonly class JwksDecorator implements JsonSerializable
 {
     public function __construct(public JWKSet $jwks)
     {
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->jwks->jsonSerialize();
     }
 }
