7cbfcf24652e6e1669337d22122e888084b38495
Marko Ivančić <marko.ivancic@srce.hr>
2024-12-05 11:07:04 +0100

    Update errors

--- src/Codebooks/ErrorsEnum.php
+++ src/Codebooks/ErrorsEnum.php
@@ -9,6 +9,10 @@ enum ErrorsEnum: string
     case InvalidRequest = 'invalid_request';
     case InvalidClient = 'invalid_client';
     case InvalidIssuer = 'invalid_issuer';
+    case InvalidMetadata = 'invalid_metadata';
+    case InvalidSubject = 'invalid_subject';
+    case InvalidTrustAnchor = 'invalid_trust_anchor';
+    case InvalidTrustChain = 'invalid_trust_chain';
     case NotFound = 'not_found';
     case ServerError = 'server_error';
     case TemporarilyUnavailable = 'temporarily_unavailable';
