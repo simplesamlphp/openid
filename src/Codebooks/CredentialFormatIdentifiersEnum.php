8b52c6ad6af0ba8dd419723a0129f402475ff7f6
Marko Ivancic <mivanci@srce.hr>
2025-04-06 19:29:46 +0200

    Update CodeBooks

---
+++ src/Codebooks/CredentialFormatIdentifiersEnum.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\OpenID\Codebooks;
+
+enum CredentialFormatIdentifiersEnum: string
+{
+    // W3C Verifiable Credentials
+    // VC signed as a JWT, not using JSON-LD
+    case JwtVcJson = 'jwt_vc_json';
+    // VC signed as a JWT, using JSON-LD
+    case JwtVcJsonLd = 'jwt_vc_json-ld';
+    // VC secured using Data Integrity, using JSON-LD, with a proof suite requiring Linked Data canonicalization
+    case LdpVc = 'ldp_vc';
+
+    // ISO mDL
+    // Mobile Security Object (MSO) which secures the mdoc data model encoded as CBOR.
+    case MsoMdoc = 'mso_mdoc';
+
+    // IETF SD-JWT VC
+    case DcSdJwt = 'dc+sd-jwt';
+}
