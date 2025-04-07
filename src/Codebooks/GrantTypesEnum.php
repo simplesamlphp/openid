5dfa8663c0812437488523d68589757d6368643c
Marko Ivancic <mivanci@srce.hr>
2025-04-07 15:18:18 +0200

    Update CodeBooks

--- src/Codebooks/GrantTypesEnum.php
+++ src/Codebooks/GrantTypesEnum.php
@@ -8,5 +8,6 @@ enum GrantTypesEnum: string
 {
     case AuthorizationCode = 'authorization_code';
     case Implicit = 'implicit';
+    case PreAuthorizedCode = 'urn:ietf:params:oauth:grant-type:pre-authorized_code';
     case RefreshToken = 'refresh_token';
 }
